acb5e22088ec6f1053bb4b3827569bc9391783d0
Fadilah Riczky <friczky@gmail.com>
2022-12-14 22:40:10 +0700

    disk and memory size

--- disk-memory-size.sh
+++ disk-memory-size.sh
@@ -2,6 +2,7 @@
 
 TOTAL_DISK=`df -h --total | awk 'END {print $2}'`
 TOTAL_MEMORY=`free --total --mega | awk 'END {print $2"MB"}'`
+# TOTAL_MEMORY=`free -h --total | awk 'END {print $2"MB"}'`
 
 echo "Total disk size: $TOTAL_DISK"
 echo "Total memory size: $TOTAL_MEMORY"
